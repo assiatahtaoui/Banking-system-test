0fda81ad5a0112463ca414367b87161b43c11778
Assia TAHTAOUI <129065960+assiatahtaoui@users.noreply.github.com>
2025-12-15 01:14:57 +0100

    Update Transaction.java

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -1,4 +1,26 @@
 package model;
 
-public class Transactiob {
+public class Transaction {
+    private String date;
+    private int amount;
+    private int balance;
+
+
+
+    public Transaction(int amount, String date, int balance) {
+        this.amount = amount;
+        this.date = date.toString();
+        this.balance = balance;
+
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+    public int getBalance() {
+        return balance;
+    }
 }
