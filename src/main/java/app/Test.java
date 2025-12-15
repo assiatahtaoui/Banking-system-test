fc4405c32b139799d2d97dad3ee65ef17edb61ed
Assia TAHTAOUI <129065960+assiatahtaoui@users.noreply.github.com>
2025-12-15 01:15:35 +0100

    Update Test.java

--- src/main/java/app/Test.java
+++ src/main/java/app/Test.java
@@ -1,4 +1,14 @@
 package app;
 
 public class Test {
+   public static void main(String[] args) {
+        Account acc = new Account();
+
+        acc.deposit(1000,"10-01-2012");
+        acc.deposit(2000,"13-01-2012");
+        acc.withdraw(500, "14-01-2012");
+
+        acc.printStatement();
+
+    }
 }
